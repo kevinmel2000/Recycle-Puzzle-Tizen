a733cf161f8b059167b9cc2b19fe7df640fbde3c
omrobbie <putu.roby@yahoo.com>
2017-04-26 15:12:54 +0800

    game object sudah bisa bergerak ke bawah

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,17 @@
+﻿public class Data
+{
+    private static int score = 0;
+
+    public static int Score
+    {
+        get
+        {
+            return score;
+        }
+
+        set
+        {
+            score = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+    float speed = 0.6f;
+    public Sprite[] img;
+
+    // Use this for initialization
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = img[Random.Range(0, img.Length)];
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(0, speed * Time.deltaTime * -1f, 0);
+        if(transform.position.y < -5f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
