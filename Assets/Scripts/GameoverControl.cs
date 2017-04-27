5b4f92be07cf1af0cfcd07ba369a1a504aad411f
omrobbie <putu.roby@yahoo.com>
2017-04-27 11:32:51 +0800

    buat scene khusus untuk GameOver

---
+++ Assets/Scripts/GameoverControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameoverControl : MonoBehaviour
+{
+    public Text score;
+
+    // Use this for initialization
+    private void Start()
+    {
+        score.text = Data.Score.ToString();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void Ulangi()
+    {
+        Data.Score = 0;
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+}
